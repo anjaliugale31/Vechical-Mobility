2bea46c338a3f9506d03c1f518e07379123cda31
Anjali Ugale <anjali30@Anjalis-MacBook-Air.local>
2024-12-20 07:55:35 +0530

    front code pushed

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import Vehicle from '../src/component/Vehicle';
+import Layout from '../src/Layout/Layout';
+import { Routes, Route } from "react-router-dom"
+
+import Issue from './component/Issue';
+import ComponentRegistration from './component/ComponentRegistrtion';
+import Payment from './component/Payment';
+import ComponetDetailForm from "./component/Forms/ComponetDetailForm";
+import VichecalDetailForm from "./component/Forms/VichecalDetailForm";
+import IssueDetailForm from './component/Forms/IssueDetailForm';
+import PaymentDetailForm from './component/Forms/PaymentDetailForm';
+
+function App() {
+  return (
+    <Routes>
+    <Route path="/" element={<Layout />}>
+      <Route path="/vichecal" element={<Vehicle />} />
+      <Route path="/issue-details" element={<Issue />} />
+      <Route path="/component-details" element={<ComponentRegistration />} />
+      <Route path="/payment" element={<Payment />} />
+      {/* Forms */}
+      <Route path="/component-details-form" element={<ComponetDetailForm />} />
+      <Route path="/issue-details-form" element={<IssueDetailForm />} />
+      <Route path="/vichel-form" element={<VichecalDetailForm />} />
+      <Route path="/payment-form" element={<PaymentDetailForm />} />
+    </Route>
+  </Routes>
+  );
+}
+
+export default App;
+
